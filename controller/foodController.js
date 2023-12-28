61e0a60a0fa82ca80fe2d1c456ea4aca796c82e3
Muhammad Hamza <mh408800@gmail.com>
2023-12-28 16:25:00 +0500

    set the file format

--- controller/foodController.js
+++ controller/foodController.js
@@ -2,34 +2,48 @@ const foodModel = require("../models/foodModel");
 const multer = require("multer");
 const path = require("path");
 
-// multer configturation
+// multer configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, "../uploads/foodImages"));
   },
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    const ext = path.extname(file.originalname);
+    const fileName = `foodImage_${Date.now()}${ext}`;
+    cb(null, fileName);
   },
 });
-const upload = multer({ storage: storage }).single("foodImage");
+
+const fileFilter = (req, file, cb) => {
+  const allowedFileTypes = /jpeg|jpg|png/;
+  const ext = path.extname(file.originalname).toLowerCase();
+  const isAllowed = allowedFileTypes.test(ext);
+  if (isAllowed) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Only jpeg, jpg, and png files are allowed!"), false);
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter }).single("foodImage");
 
 const handleFileUpload = (req, res, next) => {
   upload(req, res, function (err) {
     if (err instanceof multer.MulterError) {
       return res.json({
-        message: "File upload error. Please upload the file in PDF format only",
+        message: "File upload error. Please upload the file in JPEG, JPG, or PNG format only",
       });
     } else if (err) {
-      return res.sendError({ message: "Internal server error" }, 500);
+      return res.status(500).json({ message: "Internal server error" });
     }
     next();
   });
 };
 
 const addFood = async (req, res) => {
-  const { foodName, foodPrice, foodDescription, foodImage } = req.body;
+  const { foodName, foodPrice, foodDescription } = req.body;
   if (!foodName || !foodPrice || !foodDescription) {
-    return res.status(400).json({ message: "all fields required" });
+    return res.status(400).json({ message: "All fields are required" });
   }
   if (!req.file) {
     return res.status(400).json({ message: "Food image is required!" });
@@ -38,15 +52,17 @@ const addFood = async (req, res) => {
     const newFood = await foodModel.create({
       foodName,
       foodPrice,
-      foodImage: req.file.originalname,
+      foodImage: `../uploads/foodImages/${req.file.filename}`, // Use filename provided by multer
       foodDescription,
     });
     console.log(newFood);
     await newFood.save();
-    console.log("food saved=>", newFood);
-    return res.status(201).json({ message: "food added successfully!", food:newFood });
+    console.log("food saved =>", newFood);
+    return res
+      .status(201)
+      .json({ message: "Food added successfully!", food: newFood });
   } catch (error) {
-    console.error("error=>", error);
+    console.error("error =>", error);
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
