7b9fa3428cb0ddec5b3a6c47960e1a299e86413b
Muhammad Hamza <mh408800@gmail.com>
2023-12-29 14:55:06 +0500

    salt round

--- controller/userController.js
+++ controller/userController.js
@@ -1,23 +1,22 @@
 const userModel = require("../models/userModel");
+const bcrypt = require("bcrypt");
 const registerUser = async (req, res) => {
-  if (
-    !req.body.userName ||
-    !req.body.userEmail ||
-    !req.body.password ||
-    !req.body.password ||
-    !req.body.cnic ||
-    !req.body.phoneNumber
-  ) {
-    return res.status(400).json({ message: "all fields are required" });
-  }
   try {
-    const newUser = await userModel.create({ ...req.body });
-    if (newUser) {
-      console.log("user saved successfully!");
-      return res
-        .status(201)
-        .json({ message: "user saved successfully!", user: newUser });
+    const { userName, userEmail, phoneNumber, password, cnic } = req.body;
+    if (!userName || !userEmail || !password || !cnic || !phoneNumber) {
+      return res.status(400).json({ message: "all fields are required" });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // console.log(hashedPassword)
+    const newUser = await userModel.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+    console.log(newUser)
+    return res
+      .status(201)
+      .json({ message: "user created successfully!", user: newUser });
   } catch (error) {
     console.error("error->", error);
     return res.status(500).json({ message: "Internal Server Error" });
